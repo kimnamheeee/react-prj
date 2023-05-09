6ae99e5206e70d5c98e3e602413d19893e407330
Kim Nam Hee <103100783+kimnamheeee@users.noreply.github.com>
2023-05-09 11:12:57 +0900

    feat: add kakao login

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,6 +4,7 @@
   할 일 목록의 추가, 삭제, 완료 상태 변경 등의 기능을 구현하였습니다.
 */
 import React, { useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
 import TodoItem from "@/components/TodoItem";
 import styles from "@/styles/TodoList.module.css";
 
@@ -17,6 +18,7 @@ import {
   updateDoc,
   deleteDoc,
   orderBy,
+  where,
 } from "firebase/firestore";
 
 const todoCollection = collection(db, "todos");
@@ -27,8 +29,16 @@ const TodoList = () => {
   const [todos, setTodos] = useState([]);
   const [input, setInput] = useState("");
 
+  const { data } = useSession();
+
   const getTodos = async () => {
-    const q = query(todoCollection, orderBy("datetime", "desc"));
+    if (!data?.user?.name) return;
+
+    const q = query(
+      todoCollection,
+      where("userId", "==", data?.user?.id),
+      orderBy("datetime", "asc")
+    );
 
     const results = await getDocs(q);
     const newTodos = [];
@@ -42,7 +52,7 @@ const TodoList = () => {
 
   useEffect(() => {
     getTodos();
-  }, []);
+  }, [data]);
 
   // addTodo 함수는 입력값을 이용하여 새로운 할 일을 목록에 추가하는 함수입니다.
   const addTodo = async () => {
@@ -58,6 +68,7 @@ const TodoList = () => {
     const day = new Date().toISOString().replace("T", " ").slice(0, -5);
 
     const docRef = await addDoc(todoCollection, {
+      userId: data?.user?.id,
       text: input,
       completed: false,
       datetime: day,
@@ -105,7 +116,7 @@ const TodoList = () => {
   return (
     <div className={styles.container}>
       <h1 className="text-xl mb-4 font-bold underline underline-offset-4 decoration-wavy">
-        Todo List
+        {data?.user?.name}'s Todo List
       </h1>
       {/* 할 일을 입력받는 텍스트 필드입니다. */}
       <input
