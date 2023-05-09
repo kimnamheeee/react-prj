329fdaa9451d30ccd86df305910dbc95f05f0995
Kim Nam Hee <103100783+kimnamheeee@users.noreply.github.com>
2023-05-09 22:50:13 +0900

    feat: add google login

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,7 +4,7 @@
   할 일 목록의 추가, 삭제, 완료 상태 변경 등의 기능을 구현하였습니다.
 */
 import React, { useState, useEffect } from "react";
-import { useSession } from "next-auth/react";
+import { useSession, signOut } from "next-auth/react";
 import TodoItem from "@/components/TodoItem";
 import styles from "@/styles/TodoList.module.css";
 
@@ -165,6 +165,17 @@ const TodoList = () => {
         onClick={setAdmin}
       >
         {userinfo ? "관리자모드" : "일반모드"}
+      </button>
+      <button
+        className={`w-38
+                      justify-self-center
+                      p-1 mb-4 mr-4
+                      text-blue-500
+                      border border-blue-500 rounded
+                    hover:bg-white hover:text-blue-500`}
+        onClick={() => signOut()}
+      >
+        Sign out
       </button>
       {/* 할 일을 입력받는 텍스트 필드입니다. */}
       <input
